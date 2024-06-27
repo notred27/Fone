6cdb3fa4518aa872941316a48ea080585e0aa72b
notred27 <mikereidy95@gmail.com>
2024-06-27 16:51:02 -0400

    changed firebase access

--- src/firebase.js
+++ src/firebase.js
@@ -4,24 +4,45 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
 
+console.log(process.env)
 
 const FIREBASE_API_CONFIG = {
 
-    apiKey: process.env.REACT_APP_apiKey,
+    apiKey: "AIzaSyDDWNc4FAG_CjvyEW9sL-uiLXjsvGgRzCk",
   
-    authDomain: process.env.REACT_APP_authDomain,
+    authDomain: "joe-app-8330b.firebaseapp.com",
   
-    projectId: process.env.REACT_APP_projectId,
+    projectId: "joe-app-8330b",
   
-    storageBucket: process.env.REACT_APP_storageBucket,
+    storageBucket: "joe-app-8330b.appspot.com",
   
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    messagingSenderId: "4705277287",
   
-    appId: process.env.REACT_APP_appId,
+    appId: "1:4705277287:web:42a977395d0fef9d300bc2",
   
-    measurementId: process.env.REACT_APP_measurementId
+    measurementId: "G-WKLE7YY73N"
   
-  }
+  };
+  
+  
+
+// const FIREBASE_API_CONFIG = {
+
+//     apiKey: process.env.REACT_APP_apiKey,
+  
+//     authDomain: process.env.REACT_APP_authDomain,
+  
+//     projectId: process.env.REACT_APP_projectId,
+  
+//     storageBucket: process.env.REACT_APP_storageBucket,
+  
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  
+//     appId: process.env.REACT_APP_appId,
+  
+//     measurementId: process.env.REACT_APP_measurementId
+  
+//   }
 
 const app = initializeApp(FIREBASE_API_CONFIG);
 export const auth = getAuth(app)
